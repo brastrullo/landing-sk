57069533b6e40d73a54561b01ac47fd7e14905b1
Bradley Rastrullo <11504992+brastrullo@users.noreply.github.com>
2022-11-25 21:19:06 -0600

    update home-main-text for smaller devices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,7 +38,7 @@ if (browser) {
   <div class="return-home"/>
   <div class="sm:h-screen w-screen transition-opacity flex flex-col sm:flex-row justify-end sm:justify-between sm:items-end sm:p-20">
     <div class="h-screen sm:h-auto flex flex-col justify-end">
-      <p class="home-main-text text-7xl sm:max-w-80 mx-4 md:max-w-[40rem] font-bold mb-8 sm:mb-0" in:fade out:fly={{y:300, duration: 250, easing: quintInOut}}>"I create UI, Motion Graphics and anything we can imagine.</p>
+      <p class="home-main-text text-5xl sm:text-7xl sm:max-w-80 mx-4 md:max-w-[40rem] font-bold mb-8 sm:mb-0" in:fade out:fly={{y:300, duration: 250, easing: quintInOut}}>"I create UI, Motion Graphics and anything we can imagine.</p>
       <span class="sm:hidden text-center animate-bounce text-xs font-extralight opacity-50">scroll down</span>
     </div>
     <div class="h-screen sm:h-auto text-2xl flex flex-col justify-end sm:w-1/2 sm:bottom-20 sm:right-20 text-center sm:text-right mb-8 sm:mb-0">
